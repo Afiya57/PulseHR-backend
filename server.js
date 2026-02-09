685d9f8af9fec0be675d938793599ec5cc075b62
Afiya Hussain <aafiyahhussain57@gmail.com>
2026-02-09 19:13:31 +0500

    PulseHR-backend

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const mongoose = require('mongoose');
 require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log("MongoDB Connection Error ❌:", err))
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/database");
